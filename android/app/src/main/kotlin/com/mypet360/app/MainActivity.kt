424fef5de52b7403b07896cb64909af36917c48d
jaideep1212 <das.jaideep@gmail.com>
2026-08-04 17:47:03 +0200

    Initial scaffolding version for android version of the app

---
+++ android/app/src/main/kotlin/com/mypet360/app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.mypet360.mypet360
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ android/app/src/main/kotlin/com/mypet360/mypet360/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.mypet360.app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
